e3c2866ed0ecaebd12e50769db1a51a96b9e3b6b
Ajeet Singh <ajeets110@gmail.com>
2019-10-04 02:07:27 +0530

    [Ajeet] calcBirthYear method building

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,3 +1,11 @@
+import javax.sound.midi.Soundbank;
+import java.sql.SQLOutput;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Calendar;
+import java.util.Date;
+
+
 public class Employee {
 
     //V a r i a b l e s    d e c l a r a t i o n
@@ -5,8 +13,20 @@ public class Employee {
     private String name = "";
     private int age;
 
+    //M e t h o d s    d e c l a r a t i o n
 
-    
+    int calcBirthYear(){
+
+        Date date = new Date();
+        int year = Calendar.getInstance().get(Calendar.YEAR);
+        System.out.println(year);
+        //if ((birthDate != null) && (currentDate != null)) {
+          //  return Period.between(birthDate, currentDate).getYears();
+        //} else {
+
+        return 0;
+    }
+
 
 
 }
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,5 +1,6 @@
 public class main {
     public static void main(){
-
+        Employee e1 = new Employee();
+        e1.calcBirthYear();
     }
 }
