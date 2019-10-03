3bccc9514e4e824ed458bc74ba2da57306b485b5
Ajeet Singh <ajeets110@gmail.com>
2019-10-04 04:40:26 +0530

    [Ajeet] Variable declaration of FullTIme class and made it implement IPrintable

--- src/main/java/FullTIme.java
+++ src/main/java/FullTIme.java
@@ -1,3 +1,10 @@
-public class FullTIme extends Employee{
+public class FullTIme extends Employee implements IPrintable{
+
+    //V a r i a b l e     d e c l a r a t i o n
+    Double salary;
+    Double bonus;
+
+    
+
 
 }
