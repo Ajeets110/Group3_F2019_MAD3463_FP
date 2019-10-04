5cc0025a6629e6d0aba883081f8ccd7c8f521246
RAKESH KUMAR <rakeshkumar92215@gmail.com>
2019-10-04 04:02:07 -0400

    getters and setters

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -3,7 +3,22 @@ public class Car extends Vehicle implements IPrintable {
 private String  Color;
 private byte NumberOfSeats;
 
+// Getters and setters
 
 
+    public String getColor() {
+        return Color;
+    }
 
+    public void setColor(String color) {
+        Color = color;
+    }
+
+    public byte getNumberOfSeats() {
+        return NumberOfSeats;
+    }
+
+    public void setNumberOfSeats(byte numberOfSeats) {
+        NumberOfSeats = numberOfSeats;
+    }
 }
