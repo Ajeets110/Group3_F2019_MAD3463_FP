eb0613b1ed91b19a4ea1a0fb94967ed186a84271
RAKESH KUMAR <rakeshkumar92215@gmail.com>
2019-10-04 04:00:02 -0400

    updation

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,11 +1,7 @@
 public class Car extends Vehicle implements IPrintable {
 // Car's Additional Variable
 private String  Color;
-private enum type{
-    Sports,
-    Luxury,
-    Normal
-}
+private byte NumberOfSeats;
 
 
 
