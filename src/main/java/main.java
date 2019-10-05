b5dd5dd5649b01d053813a8b5d2449d5e2d203dc
RAKESH KUMAR <rakeshkumar92215@gmail.com>
2019-10-05 18:29:37 -0400

    Final Push

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,7 +1,18 @@
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
 public class main {
 
     public static void main(String[] args) {
-        
+
+        PrintStream out = null;
+        try {
+            out = new PrintStream(new FileOutputStream("output.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        System.setOut(out);
 
         FullTIme f1 = new FullTIme();
         f1.setName("Skrillex");
